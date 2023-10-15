38922cbabf803cf59f4f9f6970f68b1cc3013655
Edantuti <edantuti@gmail.com>
2023-10-15 09:07:54 +0530

    changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require('mongoose');
 const app = express();
-const port = 80;
+const port = 8080;
 
 app.use(express.static(__dirname + "/static/"));
 
@@ -11,21 +11,21 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.status(200).render('student.pug');
+  res.status(200).render('student.pug');
 })
 app.get('/teacher', (req, res) => {
-    res.status(200).render('teacher.pug');
+  res.status(200).render('teacher.pug');
 })
 app.get('/live', (req, res) => {
-    res.status(200).render('liveStudent.pug');
+  res.status(200).render('liveStudent.pug');
 })
 app.get('/recorded', (req, res) => {
-    res.status(200).render('recordedStudent.pug');
+  res.status(200).render('recordedStudent.pug');
 })
 app.get('/teacher/set-lecture', (req, res) => {
-    res.status(200).render('setTeacher.pug');
+  res.status(200).render('setTeacher.pug');
 })
 
 app.listen(port, () => {
-    console.log(`The application started successfully on the port ${port}`)
+  console.log(`The application started successfully on the port ${port}`)
 })
